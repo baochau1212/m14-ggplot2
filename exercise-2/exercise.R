8ddc71c1bb8f1a406b9aa7e7f39c06fc956a0307
Baochau Tran <baochautran@Baochaus-MacBook-Air.local>
2017-05-04 18:08:29 -0700

    Few Changes

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,7 +2,7 @@
 
 # Install and load `ggplot2`
 # install.packages("ggplot2") # if needed
-
+library(ggplot2)
 
 # For this exercise you will again be working with the `diamonds` data set.
 # Use `?diamonds` to review details about this data set
@@ -13,26 +13,32 @@
 
 # Draw a bar chart of the diamonds data, organized by cut
 # The height of each bar is based on the "count" (number) of diamonds with that cut
-
+ggplot(diamonds) +
+  geom_bar(mapping = aes(x = cut, stat = 'count'))
 
 # Use the `stat_count` to apply the statistical transformation "count" to the diamonds
 # by cut. You do not need a separate geometry layer!
-
+ggplot(diamonds) +
+  stat_count(mapping = aes(x = cut) )
 
 # Use the `stat_summary` function to draw a chart with a summary layer.
 # Map the x-position to diamond `cut`, and the y-position to diamond `depth`
 # Bonus: use `min` as the function ymin, `max` as the function ymax, and `median` as the function y
-
+ggplot(diamonds) +
+  stat_summary(mapping = aes(x = cut, y = depth),
+               ) 
 
 
 ## Position Adjustments
 
 # Draw a bar chart of diamond data organized by cut, with each bar filled by clarity.
 # You should see a _stacked_ bar chart.
-
+ggplot(diamonds) +
+  geom_bar(mapping = aes(x = cut, fill =  clarity))
 
 # Draw the same chart again, but with each element positioned to "fill" the y axis
-
+ggplot(diamonds) +
+  geom_bar(mapping = aes(x = cut, fill =  factor(clarity)) 
 
 # Draw the same chart again, but with each element positioned to "dodge" each other
 
